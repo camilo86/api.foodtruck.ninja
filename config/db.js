492a588cd8d704c805b00c3520d2219616670a0a
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2018-01-07 17:47:52 -0500

    Fixed typo 'NODE_ENV' check in db config

--- config/db.js
+++ config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  const dbUrl = process.env.ENV_PORT === 'test' ? process.env.DB_TEST : process.env.DB;
+  const dbUrl = process.env.NODE_ENV === 'test' ? process.env.DB_TEST : process.env.DB;
   
   mongoose.connect(dbUrl, {useMongoClient: true});
   mongoose.Promise = global.Promise;
   mongoose.connection.on('error', (error) => {
     console.error('Could not connect to db');
+    console.log(error);
     process.exit(1);
   });
 
